2d7d314bb44511cc7a31d0867fae7ffca9a3bbe5
Larissa Nguyen <larissa.nguyen@yale.edu>
2020-05-24 19:41:27 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -1,6 +1,9 @@
 def find_max_value(array)
   max_val = -10
   array.length.times do |index|
-    if 
+    if array[index] > max_val
+      max_val = array[index]
+    end
   end
+  return max_val
 end
